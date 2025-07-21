37f42fc681178be1a55382012904d94eda9b74c2
bagussatrw <129882009+bagussatrw@users.noreply.github.com>
2025-07-21 17:15:16 +0700

    Update app.py
    menambahkan TURN server gratis dari github

--- app.py
+++ app.py
@@ -138,6 +138,11 @@ elif app_mode == "Deteksi Real-Time (Webcam)":
                 {"urls": ["stun:stun.l.google.com:19302"]},
                 {"urls": ["stun:stun1.l.google.com:19302"]},
                 {"urls": ["stun:stun2.l.google.com:19302"]},
-            ]
-        }
-    )
+                
+                {
+                    "urls": ["turn:openrelay.metered.ca:80"],
+                    "username": "openrelayproject",
+                    "credential": "openrelayproject",
+                 },
+    ]
+}
